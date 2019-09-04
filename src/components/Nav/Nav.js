68a462f4b347ce109cd9bfb69822a7abb05c8ad8
Djavan Munroe <djavanm24@gmail.com>
2019-09-04 10:14:28 -0600

    Add Nav component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import 
-export const Nav = () => {
-  return (
+import LoginForm from '../LoginForm/LoginForm';
 
+export const Nav = ({ user }) => {
+  return (
+    <nav>
+    <h1>Bookify</h1>
+    { !user && <button> SIGN IN </button> }
+    { user && <button> Show Favorites </button> }
+    { user && <h2> { user.name } </h2> }
+    </nav>
   )
 }
