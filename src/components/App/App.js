f299d2563bee21a1169afb76fa41fb876d97826d
Alan Turing <freemasl.sf@gmail.com>
2019-09-04 19:10:20 -0600

    Setup SearchForm component and onChange functionality for input'

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import '../../styles/App.css';
 import Nav from '../Nav/Nav';
-import BookContainer from '../BookContainer/BookContainer'
+import BookContainer from '../BookContainer/BookContainer';
+import SearchForm from '../SearchForm/SearchForm';
 
 class App extends Component {
   constructor() {
@@ -17,6 +18,7 @@ class App extends Component {
     return (
       <main className='app'>
         <Nav user={user} />
+        <SearchForm />
         <BookContainer />
       </main>
     )
