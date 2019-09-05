9df1d06637b0579ab7757ed6f59449df0931849b
Djavan Munroe <djavanm24@gmail.com>
2019-09-05 15:08:06 -0600

    Fix error with search reducer and update reducer/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
-export const queriedBooks = (state = [], action) => {
-  switch (action.type) {
-    case 'SEARCH_BOOKS':
-      return [...action.foundBooks]
-    default:
-      return state;
-  }
-}
+import { combineReducers } from 'redux';
+import { queriedBooks } from './search';
+
+export const rootReducer = combineReducer({
+  queriedBooks
+});
