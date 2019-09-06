79b0c82d58ba9c8a1c37ba48d5099909eb344349
Alan Turing <freemasl.sf@gmail.com>
2019-09-05 18:56:38 -0600

    Add handleChange method, which updates the state on the LoginForm

--- src/containers/LoginForm/LoginForm.js
+++ src/containers/LoginForm/LoginForm.js
@@ -11,27 +11,56 @@ class LoginForm extends Component {
       password: ''
     }
   }
+
   toggleExisting = () => {
     this.setState({existingUser: !this.state.existingUser})
   }
 
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
   render() {
-    const { existingUser } = this.state;
+    const { existingUser, email, name, password } = this.state;
     const btnText = existingUser ? 'Sign up' : 'Login'
     return (
       <div>
         <button onClick={this.toggleExisting}>{btnText}</button>
       {existingUser &&
       <form>
-        <input type="text" placeholder="Email"/>
-        <input type="text" placeholder="Password"/>
+        <input 
+        type="text" 
+        placeholder="Email"
+        name="email"
+        value={email}
+        onChange={this.handleChange} />
+        <input 
+        type="password" placeholder="Password"
+        name="password"
+        value={password}
+        onChange={this.handleChange}/>
         <button>Login</button>
       </form>}
       {!existingUser &&
       <form>
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Email" />
-        <input type="text" placeholder="Create a Password" />
+        <input 
+        type="text" 
+        placeholder="Name"
+        name="name"
+        value={name}
+        onChange={this.handleChange} />
+        <input 
+        type="text" 
+        placeholder="Email"
+        name="email"
+        value={email} 
+        onChange={this.handleChange} />
+        <input 
+        type="password" 
+        placeholder="Create a Password"
+        name="password"
+        value={password}
+        onChange={this.handleChange} />
         <button>Submit</button>
       </form>}
       </div>
