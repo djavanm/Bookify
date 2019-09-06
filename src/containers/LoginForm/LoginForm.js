41844cc5204544edc6cd0c2b9e389dfb52a1f821
Alan Turing <freemasl.sf@gmail.com>
2019-09-05 18:47:09 -0600

    Set up new user and existing user inputs

--- src/containers/LoginForm/LoginForm.js
+++ src/containers/LoginForm/LoginForm.js
@@ -11,10 +11,30 @@ class LoginForm extends Component {
       password: ''
     }
   }
+  toggleExisting = () => {
+    this.setState({existingUser: !this.state.existingUser})
+  }
+
   render() {
     const { existingUser } = this.state;
+    const btnText = existingUser ? 'Sign up' : 'Login'
     return (
-      <form>hi</form>
+      <div>
+        <button onClick={this.toggleExisting}>{btnText}</button>
+      {existingUser &&
+      <form>
+        <input type="text" placeholder="Email"/>
+        <input type="text" placeholder="Password"/>
+        <button>Login</button>
+      </form>}
+      {!existingUser &&
+      <form>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Create a Password" />
+        <button>Submit</button>
+      </form>}
+      </div>
     )
   }
 }
