e5903d40646a11055bc80dc2cf8f83f60dcc6c67
Djavan Munroe <djavanm24@gmail.com>
2019-09-07 18:32:06 -0600

    Add buttons for current genres within the nav component

--- src/containers/Nav/Nav.js
+++ src/containers/Nav/Nav.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { logoutUser, showGenreFilter } from '../../actions';
+import { logoutUser, showGenreFilter, showAllFilter} from '../../actions';
 import { bindActionCreators } from 'redux';
 
-const Nav = ({ currentUser, logoutUser, home }) => {
+const Nav = ({ currentUser, logoutUser, home, genres, showGenreFilter, showAllFilter }) => {
+  const currentGenres = genres.map(genre => {
+  return <button onClick={() => showGenreFilter(genre)}>{genre}</button>
+  });
+  const showAll = <button onClick={() => showAllFilter()}>Show All</button>;
+  const allGenres = [showAll, ...currentGenres];
   return (
     <nav>
     <h1>Bookify</h1>
     { !currentUser && <Link to='/login'><button> SIGN IN </button></Link> }
     { currentUser && !home && <Link to='/my-collection'><button>Show Favorites</button></Link> }
-    { currentUser && home && <Link to='/'><button>Search Audobooks</button></Link> }
+    { currentUser && home && <Link to='/'><button>Search Audiobooks</button></Link> }
     { currentUser && <button onClick={logoutUser}>Logout</button> }
     { currentUser && <h2> { currentUser.name } </h2> }
+    { currentUser && home && <div>{ allGenres }</div> }
     </nav>
   )
 }
+export const mapStateToProps = state => ({
+  genres: state.genres
+})
 
 export const mapDispatchToProps = dispatch => (
-  bindActionCreators({ logoutUser, showGenreFilter }, dispatch)
+  bindActionCreators({ logoutUser, showGenreFilter, showAllFilter }, dispatch)
 );
 
-export default connect(null, mapDispatchToProps)(Nav);
+export default connect(mapStateToProps, mapDispatchToProps)(Nav);
