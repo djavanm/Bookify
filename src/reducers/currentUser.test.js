ecd03beb05db8ccc03cb648e5a8b2409b10e1a76
Alan Turing <freemasl.sf@gmail.com>
2019-09-08 15:18:18 -0600

    Add currentUser and favorites reducer tests

---
+++ src/reducers/currentUser.test.js
@@ -0,0 +1,38 @@
+import { currentUser } from './currentUser';
+
+describe('currentUser reducer', () => {
+  it('should return the initial state', () => {
+    const expected = null;
+
+    const result = currentUser(undefined, {});
+
+    expect(result).toEqual(expected);
+  })
+
+  it('should return the new state with a found user when FOUND_USER action is passed through', () => {
+    const expected = {
+        name: 'Djavan'
+      };
+    const actionObj = {
+      type: 'FOUND_USER',
+      foundUser: {
+        name: 'Djavan'
+      }
+    };
+
+    const result = currentUser(undefined, actionObj);
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return the new state of null when REMOVE_USER action is passed through', () => {
+    const actionObj = {
+      type: 'REMOVE_USER'
+    };
+
+    const result = currentUser(undefined, actionObj);
+
+    expect(result).toEqual(null);
+  });
+});
+
