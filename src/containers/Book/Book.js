6ed4c0c5065fe70ce268fc2d96b02f0a92ba339b
Djavan Munroe <djavanm24@gmail.com>
2019-09-08 21:08:13 -0600

    Add style to nav buttons

--- src/containers/Book/Book.js
+++ src/containers/Book/Book.js
@@ -5,6 +5,12 @@ import { connect } from 'react-redux';
 
 const Book = ({currentUser, data, toggleFavorite, favorites}) => {
   const { author_name, book_name, artwork_url, book_id } = data;
+  let shortName = book_name.split(' ')
+  if(shortName.length > 5) {
+    shortName = [shortName[0], shortName[1],shortName[2], shortName[3], shortName[4], shortName[5], shortName[6], ' ...']
+  }
+  shortName = shortName.join(' ')
+  console.log(shortName)
   const isFavorite = favorites.map(favorite => favorite.book_id).includes(book_id);
   const btn = isFavorite ? 'btn-active': 'btn'
   const favoriteBtn = currentUser ? <button className={btn} onClick={() => toggleFavorite(data, isFavorite)}>Fav</button> : <Link to='/login'><button>Fav</button></Link>
@@ -13,7 +19,7 @@ const Book = ({currentUser, data, toggleFavorite, favorites}) => {
       <img src={artwork_url} alt={`Cover for ${book_name}`} className='card-cover' />
       <section className='card-info'>
       <Link to={`/book/${book_id}`} className="link">
-        <p className='book-name'>{book_name}</p>
+        <p className='book-name'>{shortName}</p>
       </Link>
       <p className='author-name'>{author_name}</p>
       </section>
